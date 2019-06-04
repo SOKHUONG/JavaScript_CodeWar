cc018fa8fbf48cb2c93bdb12e829486cc53e0721
RinKimhun <47348954+kimkimhun3@users.noreply.github.com>
2019-06-04 14:11:45 +0700

    Create isPrime.js

---
+++ 6kyu/isPrime.js
@@ -0,0 +1,9 @@
+function isPrime(num){
+if(num<=1) return false;
+if(num<=3) return true;
+if(num%2==0||num%3==0) return false;
+for(var i =5;i*i<=num;i=i+6)
+{ if(num%i==0||num%(i+2)==0) return false;
+ }
+return true;
+}
