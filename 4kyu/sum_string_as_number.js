c902b78aeb6dfd57460e086345344d71b46a6275
RinKimhun <47348954+kimkimhun3@users.noreply.github.com>
2019-06-08 20:57:46 +0700

    Create sum_string_as_number.js

---
+++ 4kyu/sum_string_as_number.js
@@ -0,0 +1,11 @@
+function sumStrings(a,b) {
+var res = '', c= 0;
+a=a.split('');
+b=b.split('');
+while(a.length||b.length||c){
+c+=~~a.pop()+~~b.pop();
+res = c%10 + res;
+c = c > 9;
+}
+return res.replace(/^0+/,'');
+}
